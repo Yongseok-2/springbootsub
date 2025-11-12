97756b93b59c11328862e3b50ca8aa2828b0de9f
Yongseok-2 <8465867@naver.com>
2025-11-12 10:24:04 +0900

    1112

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,10 +1,7 @@
 package com.example.firstproject.entity;
 
 import com.example.firstproject.dto.ArticleForm;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,7 +14,7 @@ import lombok.ToString;
 @Setter
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
